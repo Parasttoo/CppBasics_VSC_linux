0f7273dbda57b3eaf7292e40132905a00f44499d
Parastoo <bright.sunrise33@gmail.com>
2024-08-13 23:05:12 +0330

    private and public access specifier practice

--- source.cpp
+++ source.cpp
@@ -31,9 +31,45 @@ void basic_class(void)
     account1.printmsg();
     account1.withdraw(400);
 }
+/*********************************/
+class Employee
+{
+    public:
+    string name;
+    void set_salary(double x)
+    {
+        salary = x;
+    }
+    double get_salary()
+    {
+        return salary;
+    }
 
+    void printBounes()
+    {
+        cout << "nmae: "<< name << " has "<< bounes() << " bounes." << endl;
+    }
+
+    private:
+    double salary;
+    double bounes()
+    {
+        return salary * 0.1;
+    }
+};
+
+void priv_pub_acc(void)
+{
+    Employee emp1;
+    emp1.name = "Jim";
+    emp1.set_salary(700);
+    cout << "name: " << emp1.name << " has " << emp1.get_salary() << " salary." << endl;
+    emp1.printBounes();
+}
+/*************************************/
 int main()
 {
-    basic_class();
+    //basic_class();
+    priv_pub_acc();
     return 0;
 }
